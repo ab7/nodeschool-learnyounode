aa7bd2843ec8a92334bc866b0a0c84f802d29652
Alex Baker <al3xb4k3r@gmail.com>
2016-02-09 19:24:46 -0700

    Added official solution and comments.

--- ex2/program.js
+++ ex2/program.js
@@ -5,3 +5,16 @@ for (var i = 2; i < params.length; i++) {
 }
 console.log(sum);
 
+
+// ~ Official Solution ~
+// var result = 0
+//
+// for (var i = 2; i < process.argv.length; i++)
+//   result += Number(process.argv[i])
+//
+// console.log(result)
+
+// ~ Comments ~
+// * Just noticed the absence of semi-colons. Found a style-guide that I'll try and follow:
+//   https://github.com/felixge/node-style-guide#use-semicolons.
+// * Should have stored `process.argv` in a variable.
