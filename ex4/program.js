f2ac4af3a552ae0ec8e30a69e3f7fedf8796766f
Alex Baker <al3xb4k3r@gmail.com>
2016-02-09 19:29:53 -0700

    Added official solution and comments.

--- ex4/program.js
+++ ex4/program.js
@@ -7,3 +7,17 @@ fs.readFile(file, 'utf-8', function(err, data) {
   console.log(data.split('\n').length - 1);
 });
 
+
+// ~ Official Solution ~
+// var fs = require('fs')
+// var file = process.argv[2]
+//
+// fs.readFile(file, function (err, contents) {
+//   // fs.readFile(file, 'utf8', callback) can also be used
+//   var lines = contents.toString().split('\n').length - 1
+//   console.log(lines)
+// })
+
+// ~ Comments ~
+// * Solution didn't include error handling but it seems like general practice so I'll keep using it.
+// * Declaring the `lines` variable was more explicit.
