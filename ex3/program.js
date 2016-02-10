97799923dbe55cd981888fe1223c1210f2c94755
Alex Baker <al3xb4k3r@gmail.com>
2016-02-09 19:29:39 -0700

    Added official solution and comments.

--- ex3/program.js
+++ ex3/program.js
@@ -5,3 +5,20 @@ var fs = require('fs'),
 
 console.log(split_file.length - 1);
 
+
+// ~ Official Solution ~
+// var fs = require('fs')
+//
+// var contents = fs.readFileSync(process.argv[2])
+// var lines = contents.toString().split('\n').length - 1
+// console.log(lines)
+//
+// note you can avoid the .toString() by passing 'utf8' as the
+// second argument to readFileSync, then you'll get a String!
+//
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+
+// ~ Comments ~
+// * I'm noticing that require is exactly like `import` in Python.
+// * I should have put the `- 1` at the end of the `split_file` variable.
+// * The 'utf-8' tip is nice, I'll make sure to use it moving forward.
