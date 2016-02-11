014e6af90b2dc81cade5fddcca61b612a541a3c1
Alex Baker <al3xb4k3r@gmail.com>
2016-02-10 22:31:56 -0700

    Added official solution and comments.

--- ex5/program.js
+++ ex5/program.js
@@ -13,3 +13,20 @@ fs.readdir(path, function (err, files) {
   }
 });
 
+
+// ~ Official Solution ~
+// var fs = require('fs')
+// var path = require('path')
+//
+// fs.readdir(process.argv[2], function (err, list) {
+//   list.forEach(function (file) {
+//     if (path.extname(file) === '.' + process.argv[3])
+//       console.log(file)
+//   })
+// })
+
+// ~ Comments ~
+// * I don't see `forEach` that often. Interesting warning from MDN doc page:
+//    > Note: There is no way to stop or break a forEach() loop other than by throwing
+//    > an exception. If you need such behaviour, the .forEach() method is the wrong
+//    > tool, use a plain loop instead.
