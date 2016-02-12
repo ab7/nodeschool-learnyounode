76cbcb95137021c9d73419e182583c9ae32522af
Alex Baker <al3xb4k3r@gmail.com>
2016-02-11 22:06:51 -0700

    Added official solutions and comments.

--- ex6/file_filter.js
+++ ex6/file_filter.js
@@ -18,3 +18,27 @@ Filter.prototype.filterByExt = function (path, ext, callback) {
 };
 
 module.exports = new Filter();
+
+
+// ~ Official Solution ~
+// var fs = require('fs')
+// var path = require('path')
+//
+// module.exports = function (dir, filterStr, callback) {
+//
+//   fs.readdir(dir, function (err, list) {
+//     if (err)
+//       return callback(err)
+//
+//     list = list.filter(function (file) {
+//       return path.extname(file) === '.' + filterStr
+//     })
+//
+//     callback(null, list)
+//   })
+// }
+
+// ~ Comments ~
+// * I really wanted to export an anyonymous object like in the style of `path`
+//   and `fs` so I willingly failed the 'single function' export requirement.
+//   Source: https://darrenderidder.github.io/talks/ModulePatterns/#/8
