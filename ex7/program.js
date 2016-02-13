74be24830e7ade96b2879fef232de45513846293
Alex Baker <al3xb4k3r@gmail.com>
2016-02-12 21:13:42 -0700

    Added official solution and comments.

--- ex7/program.js
+++ ex7/program.js
@@ -9,3 +9,17 @@ http.get(process.argv[2],  function (response) {
   console.log("Got error: " + e.message);
 });
 
+
+// ~ Official Solution ~
+// var http = require('http')  
+//        
+// http.get(process.argv[2], function (response) {  
+//   response.setEncoding('utf8')  
+//   response.on('data', console.log)  
+//   response.on('error', console.error)  
+// })
+
+// ~ Comments ~
+// * Docs used example with `.on` how I did it. I prefer the
+//   the style in the official solution.
+
