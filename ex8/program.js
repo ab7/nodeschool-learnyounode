0b10f6cc1346b2349b9423b9c9bf4db812ea5446
Alex Baker <al3xb4k3r@gmail.com>
2016-02-13 21:27:46 -0700

    Added official solution and comment.

--- ex8/program.js
+++ ex8/program.js
@@ -8,3 +8,21 @@ http.get(process.argv[2], function (resp) {
   }));
 });
 
+
+// ~ Official Solution ~
+// var http = require('http')  
+// var bl = require('bl')  
+//   
+// http.get(process.argv[2], function (response) {  
+//   response.pipe(bl(function (err, data) {  
+//     if (err)  
+//       return console.error(err)  
+//     data = data.toString()  
+//     console.log(data.length)  
+//     console.log(data)  
+//   }))    
+// })
+
+// ~ Comments ~
+// * Need to remember to add error handling.
+
